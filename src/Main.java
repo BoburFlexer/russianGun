1a8e70d4625b3f14e16592047cd442fb7c300d5a
BoburFlexer <bobursakirov2008@gmail.com>
2023-06-05 19:42:38 +0600

    changes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Saalo");
+        Scanner scanner = new Scanner(System.in);
+        String a = scanner.nextLine();
+        if (a.equals("exit")) {
+            System.out.println("Are you serious right now bro????");
+        }
     }
 }
