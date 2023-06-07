daeddae22b9ccf4718db59a2a78f53420916430c
BoburFlexer <bobursakirov2008@gmail.com>
2023-06-07 20:19:12 +0600

    updateCondition

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,19 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Saalo");
+        System.out.println("Choose your number.[1,7]");
         Scanner scanner = new Scanner(System.in);
-        String a = scanner.nextLine();
-        if (a.equals("exit")) {
-            System.out.println("Are you serious right now bro????");
+        int userNumber = scanner.nextInt();
+        Random random = new Random();
+        int randomNumber = random.nextInt(1, 7);
+        System.out.println("(●_-)–ε/̵͇̿/’̿’̿ ̿ ̿ ̿ \uD83D\uDCA5");
+        System.out.println(randomNumber);
+        if (userNumber == randomNumber) {
+            System.out.println("You died");
+        } else if (userNumber != randomNumber) {
+            System.out.println("You alive");
         }
     }
 }
